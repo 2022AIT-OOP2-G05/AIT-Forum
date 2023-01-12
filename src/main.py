31ef7b67dc10b02731b40217537f92063bed9f9c
TaikiShimada <k21064kk@aitech.ac.jp>
2023-01-12 14:40:50 +0900

    add: /<授業名>で詳細画面を渡す関数と授業名に応じてoutput/jsonを渡す関数の追加

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from crypt import methods
+from flask import Flask, jsonify, render_template, request
+import json
 
 #flaskの初期化
 app = Flask(__name__)
@@ -17,6 +19,29 @@ def detail():
 def form():
     
     return render_template('form.html')
+    
+@app.route('/')
+@app.route('/<subject>')
+def subject(subject):
+    return render_template('detail.html')
+
+
+
+
+
+@app.route('/api')
+@app.route('/api/detail')
+@app.route('/api/detail/<subject>', methods=['GET'])
+def subject_json(subject):
+
+    with open('src/data/json/items/'+subject+'/output/'+subject+'.json') as f:
+    
+        json_data = json.load(f)
+
+
+    return jsonify(json_data)
+
+
     
 if __name__ == "__main__":
     app.run(debug=True)
